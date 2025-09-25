840bbc987c37b45647942f4d730c8d47b5f35b0b
BorjaRodriguezz <borjard7703@gmail.com>
2025-09-25 08:52:22 +0200

    second commit

---
+++ src/ejerciciosBloque1/pideUsuarioTresNumeros.java
@@ -0,0 +1,22 @@
+package ejerciciosBloque1;
+
+import javax.swing.JOptionPane;
+
+public class pideUsuarioTresNumeros {
+
+	public static void main(String[] args) {
+		// Petición de un número entero
+		String stringNumero = JOptionPane.showInputDialog("Introduzca un número entero");
+		int numero = Integer.parseInt(stringNumero);
+
+		// Petición de un número flotante
+		String stringNumeroFlotante = JOptionPane.showInputDialog("Introduzca un número flotante");
+		float numeroFlotante = Float.parseFloat(stringNumeroFlotante);
+		
+		// Petición de un número doble
+		String stringNumeroDoble = JOptionPane.showInputDialog("Introduzca un número doble");
+		double numeroDoble = Double.parseDouble(stringNumeroDoble);
+		System.out.print("Valor de numero: " + numero + " Valor de numeroFlotante: " + numeroFlotante + " Valor de numeroDoble: " + numeroDoble);
+	}
+
+}
