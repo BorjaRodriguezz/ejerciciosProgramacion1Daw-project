5a5f7b2f38c2bab06622f599712b287435c90785
BorjaRodriguezz <borjard7703@gmail.com>
2025-10-05 20:21:57 +0200

    (fix) arreglo de error en acumuluacion de monedas en el ej04

--- src/bloque3Capitulo2/Ej04MaquinaMonedas.java
+++ src/bloque3Capitulo2/Ej04MaquinaMonedas.java
@@ -24,17 +24,23 @@ public class Ej04MaquinaMonedas {
 		} else if (cambio == 0) {
 			System.out.println("Gracias por su compra");
 		} else {
-			System.out.println("Cambio a devolver: " + cambio + "unidades");
-			int moneda100 = 100;
-			int moneda50 = 50;
-			int moneda20 = 20;
-			int moneda5 = 5;
-			int moneda1 = 1;
-			System.out.println("Cantidad de monedas de 100: " + moneda100);
-			System.out.println("Cantidad de monedas de 500: " + moneda50);
-			System.out.println("Cantidad de monedas de 20: " + moneda20);
-			System.out.println("Cantidad de monedas de 5: " + moneda5);
-			System.out.println("Cantidad de monedas de 1: " + moneda1);
+			int moneda100 = cambio / 100;
+			cambio = cambio % 100;
+			int moneda50 = cambio / 50;
+			cambio = cambio % 50;
+			int moneda20 = cambio / 20;
+			cambio = cambio % 20;
+			int moneda5 = cambio / 5;
+			cambio = cambio % 5;
+			int moneda1 = cambio / 1;
+			cambio = cambio % 1;
+			System.out.println("Su cambio es: ");
+			System.out.println("\n Cantidad de monedas de 100: " + moneda100);
+			System.out.println("\n Cantidad de monedas de 500: " + moneda50);
+			System.out.println("\n Cantidad de monedas de 20: " + moneda20);
+			System.out.println("\n Cantidad de monedas de 5: " + moneda5);
+			System.out.println("\n Cantidad de monedas de 1: " + moneda1);
+			System.out.println("\n Gracias por su compra");
 		}
 		
 		sc.close();
