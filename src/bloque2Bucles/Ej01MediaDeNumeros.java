a1ac8978387f9b2dd288ed3b1f2e43438521c63b
BorjaRodriguezz <borjard7703@gmail.com>
2025-10-07 11:30:03 +0200

    Nada nuevo

--- src/bloque2Bucles/Ej01MediaDeNumeros.java
+++ src/bloque2Bucles/Ej01MediaDeNumeros.java
@@ -7,6 +7,7 @@ public class Ej01MediaDeNumeros {
 	public static void main(String[] args) {
 		Scanner  sc = new Scanner(System.in);
 		
+		int suma;
 		int pedir;
 		
 		System.out.println("Indica cuantos numeros desea introducir: ");
@@ -16,8 +17,6 @@ public class Ej01MediaDeNumeros {
 			System.out.println("Introduzca el numero " + i + " : ");
 			int numero  = sc.nextInt();
 		}
-		
-		float media = ;
 		
 		sc.close();
 
