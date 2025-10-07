77067b2d919d83deb8859f16355234ed7189c800
BorjaRodriguezz <borjard7703@gmail.com>
2025-10-07 11:27:50 +0200

    (feat) Subida de bloque 2 Bucles

---
+++ src/bloque2Bucles/Ej01MediaDeNumeros.java
@@ -0,0 +1,26 @@
+package bloque2Bucles;
+
+import java.util.Scanner;
+
+public class Ej01MediaDeNumeros {
+
+	public static void main(String[] args) {
+		Scanner  sc = new Scanner(System.in);
+		
+		int pedir;
+		
+		System.out.println("Indica cuantos numeros desea introducir: ");
+		pedir = sc.nextInt();
+		
+		for (int i = 1; i <= pedir; i++) {
+			System.out.println("Introduzca el numero " + i + " : ");
+			int numero  = sc.nextInt();
+		}
+		
+		float media = ;
+		
+		sc.close();
+
+	}
+
+}
