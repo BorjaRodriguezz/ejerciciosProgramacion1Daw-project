399ab6b59cc592fcc8f71bbd4d32585392c6d1d3
BorjaRodriguezz <borjard7703@gmail.com>
2025-11-20 19:03:30 +0100

    Subida de ejercicios bloque 09

--- src/capitulo04/ejercicios/bloque09_MasterSobreArrays/Ej03_DNI.java
+++ src/capitulo04/ejercicios/bloque09_MasterSobreArrays/Ej03_DNI.java
@@ -11,8 +11,11 @@ public class Ej03_DNI {
 		System.out.println("Introduce tu número de DNI sin letra: ");
 		int dni = sc.nextInt();
 		
+		//Nétodo + resultado
+		char letraDni = letraDNI(dni);
+		System.out.println("Su DNI completo es: " + dni + letraDni);
 		
-
+		sc.close();
 	}
 	
 	//Método para calcular letra del DNI
@@ -23,7 +26,7 @@ public class Ej03_DNI {
 	            'N','J','Z','S','Q','V','H','L','C','K','E'
 		};
 		
-		int resto = dni % 23; //Para que este entre 0 y 22 en el array
+		int resto = dni % 23; //Para que esté entre 0 y 22 en el array
 		return letras[resto];
 	}
 
